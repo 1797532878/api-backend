6dc5fd63544fb0bd3cc282588422e62713546f9c
chenx <1797532878@qq.com>
2023-02-11 10:43:18 +0800

    代码同步

---
+++ src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.InterfaceInfo;
+
+/**
+* @author cc
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-01-30 20:35:12
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/project/service/InterfaceInfoService.java
@@ -0,0 +1,16 @@
+package com.yupi.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.yupi.project.model.entity.Post;
+
+/**
+* @author cc
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2023-01-30 20:35:12
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+    void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add);
+
+}
